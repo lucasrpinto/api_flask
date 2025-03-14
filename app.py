e734fc687fed62d45c1881790e390e08d562603b
lucasrpinto <lucasrop22@gmail.com>
2025-03-14 16:55:48 -0300

    Finalizado api flask

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 tasks = []
 task_id_control = 1
 
-# Method POST
+# Method POST (Inclui uma nova task)
 @app.route('/tasks', methods=['POST'])
 def create_task():
     global task_id_control
@@ -19,7 +19,7 @@ def create_task():
     print(tasks)
     return jsonify({"message": "Nova tarefa criada com sucesso"})
 
-# Method GET
+# Method GET (Visualiza todas tasks)
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
     task_list = [task.to_dict() for task in tasks]
@@ -30,14 +30,45 @@ def get_tasks():
             }
     return jsonify(output)
 
+# Method GET (Visualiza somente uma task)
 @app.route('/tasks/<int:id>', methods=['GET'])
 def get_task(id):
     for t in tasks:
         if t.id == id:
             return jsonify(t.to_dict())
     
-    return jsonify({"message": "Nao foi possivel encontrar a atividade"}), 404
+    return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
 
+# Methdd PUT (Atualiza uma task)
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+    print(task)
+    if task == None:
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso!"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t 
+            break
+    if not task:
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+    
+    tasks.remove(task)
+    return jsonify({"message": "Tarfea deletada com sucesso!"})
 
 
 if __name__ == "__main__":
