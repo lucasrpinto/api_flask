9879556a0c6a8aa487ec605d2a317a036482748d
lucasrpinto <lucasrop22@gmail.com>
2025-03-17 21:59:34 -0300

    Adicionado teste para GET ALL e ID

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def create_task():
     task_id_control += 1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 # Method GET (Visualiza todas tasks)
 @app.route('/tasks', methods=['GET'])
--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ import requests
 BASE_URL = 'http://127.0.0.1:5000'
 tasks = []
 
+# Teste do Create
 def test_create_task():
     new_task_data = {
         "title": "Nova Tarefa",
@@ -12,3 +13,23 @@ def test_create_task():
     }
     response = requests.post(f"{BASE_URL}/tasks", json=new_task_data)
     assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+    assert "id" in response_json
+    tasks.append(response_json['id'])
+
+# Teste do Read (GET(ALL) e GET(ID))
+def test_get_tasks():
+    response = requests.get(f"{BASE_URL}/tasks")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "tasks" in response_json
+    assert "total_tasks" in response_json
+
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert task_id == response_json['id']
